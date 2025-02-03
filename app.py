e3fb1ede0c2c907a4a15debdf9cd84ffceef06c3
Jason Joannou <jjoannou@bscglobal.com>
2025-02-03 14:36:38 +0200

    Updated app to use imports from extentions

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 import os
 from extentions import db, migrate
 
@@ -16,14 +14,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "test
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database and migration objects
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db.init_app(app)
+migrate.init_app(app, db)
 
 # Import your models after initializing db and migrate
 from src.models.user import User
-from src.models.competition import Competition
-from src.models.registration import Registration
-from src.models.competition_metadata import DanceStyle, DanceGroup
+from src.models.conversation import Conversation
+
+# For future use
+# from src.models.competition import Competition
+# from src.models.registration import Registration
+# from src.models.competition_metadata import DanceStyle, DanceGroup
 
 # routers
 from src.routes.whatsapp import whatsapp_bp
