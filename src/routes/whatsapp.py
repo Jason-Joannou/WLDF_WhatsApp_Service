b9916ea6e90ad067fa7f55102504de5bb9d5787b
Jason Joannou <jjoannou@bscglobal.com>
2025-02-03 15:57:55 +0200

    Working whatsapp router

--- src/routes/whatsapp.py
+++ src/routes/whatsapp.py
@@ -1,18 +1,34 @@
 from flask import Blueprint, request
-from src._utils.router import state_router
 from src._utils.twilio_client import TwilioClient
+from src.database.database_factory import DatabaseFactory
+from src._utils.conversation_manager import ConversationManager
+from src._utils.helper import format_number
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 whatsapp_bp = Blueprint("whatsapp", __name__)
 
 BASE_ROUTE = "/whatsapp"
 
-
 @whatsapp_bp.route(BASE_ROUTE, methods=["POST"])
 async def whatsapp() -> str:
+    db_session = DatabaseFactory.create_database(
+    db_type=os.getenv("DATABASE_TYPE"),
+    connection_params=os.getenv("DATABASE_URL"))
+
+    conversation_manager = ConversationManager(db_session)
     incoming_msg = request.values.get("Body", "")
     from_number = request.values.get("From", "")
-    print(from_number, 'This is from number')
-
-    state = await state_router(from_number)
-
-    return TwilioClient().send_template_message(to=from_number, state=state)
+    formatted_number = format_number(from_number=from_number)
+    
+    async with db_session() as session:
+        async with session.begin():
+            conversation_manager = ConversationManager(session)
+            response = await conversation_manager.handle_message(formatted_number, incoming_msg)
+            
+            return await TwilioClient().send_template_message(
+                to=from_number, 
+                state=response
+            )
