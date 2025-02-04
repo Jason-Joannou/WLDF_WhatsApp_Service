c04beb666e17b7b41623be7566c2423c89c6474a
Jason Joannou <jjoannou@bscglobal.com>
2025-02-04 11:20:46 +0200

    Added new roles to enums

--- src/models/enums.py
+++ src/models/enums.py
@@ -4,12 +4,14 @@ class UserType(str, Enum):
     STUDIO_HEAD = "studio_head"
     PARENT = "parent"
     DANCER = "dancer"
+    ADMIN = "admin"
     UNKNOWN = "unknown"
 
 class ConversationState(str, Enum):
     START = "start"
     REGISTRATION = "registration"
     USER_TYPE_SELECTION = "user_type_selection"
+    STUDIO_HEAD_AUTHENTICATION = "studio_head_authentication"
     STUDIO_HEAD_MENU = "studio_head_menu"
     PARENT_MENU = "parent_menu"
     DANCER_MENU = "dancer_menu"
